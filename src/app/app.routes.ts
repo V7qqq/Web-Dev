b7035c79b17332e44ded3ac1260f8682ec0a1c9a
V7qqq <rodion.titov7q@gmail.com>
2026-02-17 02:07:28 +0500

    Initial commit for new repository

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Catalog } from './pages/catalog/catalog';
+import { Contacts } from './pages/contacts/contacts';
+export const routes: Routes = [
+  { path: '', component: Catalog },
+  { path: 'contacts', component: Contacts },
+];
