93799400b3564666ea22b187bffd85b0e83a7c65
V7qqq <77528425+V7qqq@users.noreply.github.com>
2026-02-17 03:05:22 +0500

    task fixed

--- Lab4/task2/online-store/src/server.ts
+++ Lab4/task2/online-store/src/server.ts
@@ -53,14 +53,11 @@ app.use((req, res, next) => {
  */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
   const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
-
+  app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
+
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
